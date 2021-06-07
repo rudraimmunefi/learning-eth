dcd5d6cb0039e9b957d18f08c69dd759522b876f
Rudra Singh <65858900+rudrapwn@users.noreply.github.com>
2021-06-07 14:46:36 +0530

    removed unnecessary func

--- hello.sol
+++ hello.sol
@@ -12,9 +12,5 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-
-    function getMessage() public view returns (string) {
-        return message;
-    }
     
 }
